8ba5093f7a44b1d5294f4af9be100c702a167204
appu1232 <aditya.vad@gmail.com>
2020-12-24 15:57:55 -0500

    recursive search + include/exclude filters
    make find images more robust with the ability to recursively search for images in a directory as well as specify extensions to filter for and filter out

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import argparse, imagehash, time, os
+import glob
 from PIL import Image
 from pprint import pprint
 from multiprocessing import Pool
 from db import Database
 
-from dotenv import load_dotenv, find_dotenv
-from pathlib import Path
+from dotenv import load_dotenv
+
+
 load_dotenv(verbose=True)
 
 class GuyaEye:
@@ -16,6 +18,9 @@ class GuyaEye:
         self.images = self.kwargs['images']
         self.compare = self.kwargs['compare']
         self.threads = self.kwargs['threads']
+        self.recursive = self.kwargs['recursive']
+        self.include_extensions = self.kwargs['include_extensions'].split(",") if self.kwargs['include_extensions'] else False
+        self.exclude_extensions = self.kwargs['exclude_extensions'].split(",") if self.kwargs['exclude_extensions'] else False
 
         if self.images: self.create_database()
         elif self.compare: self.compare_image()
@@ -23,19 +28,26 @@ class GuyaEye:
 
     def create_database(self):
         s = time.time()
-        for dir, subdirs, files in os.walk(self.images):
-            for i in range(len(files)): files[i] = os.path.join(self.images, files[i])
-            pool = Pool(self.threads)
-            
-            hashes = pool.map(self.hash_image, files)
-            
-            self.db = Database()
-            self.db.truncate()
-            for hash in hashes:
-                img, h = hash
-                self.db.store_encoding(img, h)
+        image_files = []
+        for image_file in glob.iglob(self.images + "/**", recursive=self.recursive):
+            if not os.path.isfile(image_file):
+                continue
+            elif self.include_extensions and not any(image_file.endswith(ext) for ext in self.include_extensions):
+                continue
+            elif self.exclude_extensions and any(image_file.endswith(ext) for ext in self.exclude_extensions):
+                continue
+            image_files.append(os.path.join(self.images, image_file))
+        pool = Pool(self.threads)
+
+        hashes = pool.map(self.hash_image, image_files)
+
+        self.db = Database()
+        self.db.truncate()
+        for hash in hashes:
+            img, h = hash
+            self.db.store_encoding(img, h)
         e = time.time()
-        print('[ OK ] Took {:,.2f} seconds, {:,.2f} sec/image'.format(e - s, (e -s ) / len(hashes)))
+        print(f'[ OK ] Took {e - s:,.2f} seconds, {(e -s ) / len(hashes):,.2f} sec/image')
     
     def compare_image(self):
         """
@@ -43,15 +55,17 @@ class GuyaEye:
             A distance of 0 is an exact match.
         """
         self.db = Database()
-        l, h = self.hash_image(self.compare)
+        _, h = self.hash_image(self.compare)
         res = self.db.compare_encoding(h)
         pprint(res)
 
-    def hash_image(self, image):
+    @staticmethod
+    def hash_image(image):
         img = Image.open(image)
         return (image, imagehash.average_hash(img, hash_size=16))
 
-    def compare_hash(self, h1, h2):
+    @staticmethod
+    def compare_hash(h1, h2):
         return abs(h1 - h2)
 
 if __name__ == "__main__":
@@ -59,6 +73,9 @@ if __name__ == "__main__":
     args.add_argument('--images', type=str, default=False, help='Load directory of images into a database')
     args.add_argument('--compare', type=str, default=False, help='Compare image to the database')
     args.add_argument('--threads', type=int, default=4, help='Number of threads to use when generating the database')
+    args.add_argument('--recursive', action="store_true", default=False, help='Recursively search specified images directory when loading to database')
+    args.add_argument('--include-extensions', type=str, default=False, help='Comma separated list of extensions that should be filtered for when searching for images')
+    args.add_argument('--exclude-extensions', type=str, default=False, help='Comma separated list of extensions that should be filtered out when searching for images')
     args = args.parse_args()
 
     GuyaEye(**vars(args))
